71f663029a70183aebd5585c0e9e397153f7346f
jsriwongsa <jsriwongsa@hawk.iit.edu>
2023-03-17 12:56:15 -0500

    Add argparse

--- phys585_arxiv.py
+++ phys585_arxiv.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 
 import arxiv
+import argparse
 import sys
 
-query = sys.argv[1]
+parser = argparse.ArgumentParser(description='Search for a title on arxiv.')
+parser.add_argument('-q', '--query', help='Search term or query.', required=True)
+args = vars(parser.parse_args())
+
+if len(sys.argv) < 1:
+    parser.print_help(sys.stderr)
+    sys.exit(1)
+
+query = args['query']
 
 print(f"Search Query Term: {query}")
-print("--" * 40)
+print("--" * 4)
 
 find = arxiv.Search(
     query = query,
